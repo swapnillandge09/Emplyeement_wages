fcfac94a97490b6e23f1be6db9949ba7c02c0762
swapnilandge09 <swapnillandge1507@gmail.com>
2024-10-22 17:23:38 +0530

    Daily_wages

--- employee.py
+++ employee.py
@@ -7,3 +7,10 @@ if attendance == 1:
     print("Employee is Present")
 else:
     print("Employee is Absent")
+    
+#uc2 : daily Wages
+wage_per_hour = 20
+full_day_hour = 8
+daily_wage = wage_per_hour * full_day_hour
+print(f"Daily Employee Wage: {daily_wage}")
+
