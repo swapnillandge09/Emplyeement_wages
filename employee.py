a3ad52f25be8917a452868e036422e09eabc67e2
swapnilandge09 <swapnillandge1507@gmail.com>
2024-10-22 17:14:20 +0530

    Attendance

--- employee.py
+++ employee.py
@@ -1,1 +1,9 @@
 print("Welcome to Employee Wage Computation on Master Branch")
+
+import random
+# UC1: Check Employee Attendance
+attendance = random.choice([0, 1])
+if attendance == 1:
+    print("Employee is Present")
+else:
+    print("Employee is Absent")
