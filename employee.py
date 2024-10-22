689399e588f3b7347ee8357f3011e3d8e2c6e89a
swapnilandge09 <swapnillandge1507@gmail.com>
2024-10-22 17:26:52 +0530

    part-time

--- employee.py
+++ employee.py
@@ -14,3 +14,7 @@ full_day_hour = 8
 daily_wage = wage_per_hour * full_day_hour
 print(f"Daily Employee Wage: {daily_wage}")
 
+# UC3: Part-Time Employee Wage
+part_time_hour = 4
+part_time_wage = wage_per_hour * part_time_hour
+print(f"Part-time Employee Wage: {part_time_wage}")
